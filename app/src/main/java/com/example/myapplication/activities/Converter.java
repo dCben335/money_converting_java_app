6db0b05f471207c087c4fa9166910617f62dbc4c
dCben335 <benoit.cabocel.335@gmail.com>
2023-12-04 21:21:22 +0100

    Started creating select for the converting activity

--- app/src/main/java/com/example/myapplication/activities/Converter.java
+++ app/src/main/java/com/example/myapplication/activities/Converter.java
@@ -1,7 +1,13 @@
 package com.example.myapplication.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.globals.ApiCall;
 import com.example.myapplication.R;
@@ -12,12 +18,54 @@ import org.json.JSONObject;
 
 
 public class Converter extends Main {
+    private Spinner firstSpinner;
+    private Spinner secondSpinner;
+    private String[] selectOptions = {"1", "2", "3", "4"};
+    private int currentOptionOne = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loadContent(this, R.layout.converter);;
+        loadContent(this, R.layout.converter);
 
-        handleAPIData();
+
+        firstSpinner = findViewById(R.id.first_options);
+        secondSpinner = findViewById(R.id.second_options);
+        setSpinner(firstSpinner, selectOptions);
+        setSpinner(firstSpinner, selectOptions);
+
+        Button submitBtn = findViewById(R.id.submit_btn);
+        retrieveSpinnerValue(firstSpinner);
+    }
+
+    public void handleConvert() {
+
+    }
+
+    protected void setSpinner(Spinner spinner, String[] options) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+     this,
+            android.R.layout.simple_spinner_item,
+            options
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+        });
+    }
+
+    public void retrieveSpinnerValue(Spinner spinner) {
+        String selectedValue = spinner.getSelectedItem().toString();
     }
 
     public void handleAPIData() {
@@ -33,7 +81,7 @@ public class Converter extends Main {
                 }
             }
             public void onError(String errorMessage) {
-                // Handle API call error
+                Toast.makeText(Converter.this, "Error on Fetching, Retry Later", Toast.LENGTH_SHORT).show();
             }
         }, Converter.this);
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@
 
 plugins {
     id("com.android.application") version "8.1.1" apply false
-
 }
